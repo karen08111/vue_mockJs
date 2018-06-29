1d1d88d3b5ad02914ee177c075d4d91d7b9fa6a7
karen08111 <karen08111@sina.com>
2018-06-29 11:57:38 +0800

    vue搭建mock.js
    按着手册一步一步就成了

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="hello">
+   	<ul>
+   		<li v-for=" item in list">
+   			<p>名字{{item.author_name}}</p>
+   			<p>标题{{item.title}}</p>
+   			<img :src="item.thumbnail_pic_s"/>
+   		</li>
+   	</ul>
+  </div>
+</template>
+
+<script>
+	import api from './../axios/api.js'
+	
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+			list:[]
+    }
+  },
+  created(){
+	api.JH_news('/news/index', 'type=top&key=123456')
+	  .then(res => {
+	    this.list=res.articles;
+			console.log(this.list);
+	  });
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
